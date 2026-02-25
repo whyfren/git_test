07387a3fb531b0019e598192e9f91e563c211563
DoubleYuV <devmagang1123@gmail.com>
2026-02-25 22:21:54 +0700

    Fixing Player Movement

--- scripts/player/Player.gd
+++ scripts/player/Player.gd
@@ -8,7 +8,7 @@ var velocity = Vector2.ZERO
 var UP = Vector2.UP # Arah atas untuk fungsi move_and_slide
 
 func _ready():
-	print(GameManager)
+	pass
 
 func _physics_process(delta):
 	# 1. Gravitasi
@@ -16,15 +16,15 @@ func _physics_process(delta):
 
 	# 2. Input Horizontal
 	var direction = 0
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("maju"):
 		direction += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("mundur"):
 		direction -= 1
 	
 	velocity.x = direction * speed
 
 	# 3. Melompat
-	if is_on_floor() and Input.is_action_just_pressed("ui_select"):
+	if is_on_floor() and Input.is_action_just_pressed("lompat"):
 		velocity.y = jump_force
 
 	# 4. Pergerakan & Tabrakan (Penting: move_and_slide di Godot 3 butuh arah UP)
